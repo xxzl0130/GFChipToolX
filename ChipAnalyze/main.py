7acb16862c94d2c748d362841fc121489eb28d71
xxzl0130 <zaxs0130@gmail.com>
2020-05-31 14:10:59 +0800

    update

--- ChipAnalyze/main.py
+++ ChipAnalyze/main.py
@@ -48,4 +48,28 @@ for it in table:
         else:
             fout.write(',')
     fout.write('\n')
+fout.close()
+fout = open('table.md', 'w')
+fout.write('| ')
+for f in file_list:
+    fout.write(f + ' |')
+fout.write('  \n| ')
+for i in range(0,len(file_list)):
+    fout.write(' --- |')
+fout.write('  \n')
+for it in table:
+    fout.write('| ')
+    for i in range(len(file_list)):
+        if file_list[i] == it[0]:
+            if it[0] in ['2B14', 'AGS-30', 'QLZ-04',]:
+                fout.write(' ![%d1] |' % it[1])
+            else:
+                fout.write(' ![%d2] |' % it[1])
+        else:
+            fout.write('     |')
+    fout.write('  \n')
+fout.write('\n')
+for i in range(12,40):
+    fout.write('[%d1]:./img/o%d.png  \n' % (i,i))
+    fout.write('[%d2]:./img/b%d.png  \n' % (i,i))
 
